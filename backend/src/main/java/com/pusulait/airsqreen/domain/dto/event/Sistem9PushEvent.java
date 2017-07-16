130164f9a94b01858497af34fbdd8ca3787841bb
benan <aktas.benan@gmail.com>
2017-07-16 13:44:01 +0300

    Sistem9PushEvent entity details

--- backend/src/main/java/com/pusulait/airsqreen/domain/dto/event/Sistem9PushEvent.java
+++ backend/src/main/java/com/pusulait/airsqreen/domain/dto/event/Sistem9PushEvent.java
@@ -3,6 +3,7 @@ package com.pusulait.airsqreen.domain.dto.event;
 import com.pusulait.airsqreen.config.constants.Constants;
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -15,5 +16,18 @@ import javax.persistence.Table;
 @Entity
 @DiscriminatorValue(value = "SISTEM9_PUSH")
 public class Sistem9PushEvent extends BaseEvent {
+
+    @Column(name = "ACTION_ID")
+    private String actionId;
+
+    @Column(name = "DEVICE_ID")
+    private Integer deviceId;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
 
 }
