04a3f3550741504ba4bc2772bfc1a4eddb81d2d2
benan <aktas.benan@gmail.com>
2017-07-17 11:02:00 +0300

    publisher update

--- backend/src/main/java/com/pusulait/airsqreen/repository/campaign/PublisherRepository.java
+++ backend/src/main/java/com/pusulait/airsqreen/repository/campaign/PublisherRepository.java
@@ -1,13 +1,20 @@
 package com.pusulait.airsqreen.repository.campaign;
 
 import com.pusulait.airsqreen.domain.campaign.Publisher;
-import com.pusulait.airsqreen.domain.campaign.Section;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /**
  * Created by bhdr on 17.07.2017.
  */
 @Repository
 public interface PublisherRepository extends JpaRepository<Publisher, Long> {
+
+    @Query("select u from Publisher u where u.id = :id")
+    Optional<Publisher> findById(@Param(value = "id") Long id);
+
 }
