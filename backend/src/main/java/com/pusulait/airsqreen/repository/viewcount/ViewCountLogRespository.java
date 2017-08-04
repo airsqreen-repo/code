d4d56fb45dcc2d511a326e9fa5a9034869dde705
İbrahim YILDIZ <yildizib@gmail.com>
2017-08-04 11:20:12 +0300

    gösterim sayma servisi temeli...

--- backend/src/main/java/com/pusulait/airsqreen/repository/viewcount/ViewCountLogRespository.java
+++ backend/src/main/java/com/pusulait/airsqreen/repository/viewcount/ViewCountLogRespository.java
@@ -1,7 +1,14 @@
 package com.pusulait.airsqreen.repository.viewcount;
 
+import com.pusulait.airsqreen.domain.viewcount.ViewCountLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
+
 /**
  * Created by yildizib on 03/08/2017.
  */
-public interface ViewCountLogRespository {
+@Repository
+@RestResource(exported = false)
+public interface ViewCountLogRespository extends JpaRepository<ViewCountLog, Long> {
 }
