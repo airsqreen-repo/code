185722922d2bc1f64124e92555825b85e129cf31
İbrahim YILDIZ <yildizib@gmail.com>
2017-08-04 14:34:50 +0300

    ...

--- backend/src/main/java/com/pusulait/airsqreen/service/viewcount/impl/ViewCountServiceImpl.java
+++ backend/src/main/java/com/pusulait/airsqreen/service/viewcount/impl/ViewCountServiceImpl.java
@@ -9,6 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Date;
 import java.util.List;
 
+import static com.pusulait.airsqreen.util.ViewCountUtil.checkParams;
+
 /**
  * Created by yildizib on 03/08/2017.
  */
@@ -19,43 +21,74 @@ public class ViewCountServiceImpl implements ViewCountService {
     @Transactional
     @Override
     public String save(String campaignId, String sectionId) {
-        return null;
+        String result = null;
+        if (checkParams(campaignId, sectionId)) {
+            throw new NullPointerException("campaignId, sectionId   NULL veya bos olamaz!");
+        }
+        return result;
     }
 
     @Transactional
     @Override
     public String save(String campaignId, String deviceId, String actionId, String sectionId, String backendTrackUrl) {
-        return null;
+        String result = null;
+        if (checkParams(campaignId, deviceId, actionId, sectionId)) {
+            throw new NullPointerException("campaignId, deviceId, actionId, sectionId    NULL veya bos olamaz!");
+        }
+        return result;
     }
 
     @Override
     public String getTrackToken(String campaignId, String sectionId) {
-        return null;
+        String result = null;
+        if (checkParams(campaignId, sectionId)) {
+            throw new NullPointerException("campaignId, sectionId   NULL veya bos olamaz!");
+        }
+        return result;
     }
 
     @Transactional
     @Override
     public void incrementViewCount(String trackToken) {
-
+        if (checkParams(trackToken)) {
+            throw new NullPointerException("token   NULL veya bos olamaz!");
+        }
     }
 
     @Override
     public List<ViewCountDTO> getTotalCount(String token) {
-        return null;
+        List<ViewCountDTO> result = null;
+        if (checkParams(token)) {
+            throw new NullPointerException("token, start, end  NULL veya bos olamaz!");
+        }
+        return result;
     }
 
     @Override
     public List<ViewCountDTO> getTotalCount(String campaignId, String sectionId) {
-        return null;
+        List<ViewCountDTO> result = null;
+        if (checkParams(campaignId, sectionId)) {
+            throw new NullPointerException("campaignId, sectionId   NULL veya bos olamaz!");
+        }
+        return result;
     }
 
     @Override
     public List<ViewCountDTO> getTotalCountWithDateRange(String token, Date start, Date end) {
-        return null;
+        List<ViewCountDTO> result = null;
+        if (checkParams(token, start, end)) {
+            throw new NullPointerException("token, start, end   NULL veya bos olamaz!");
+        }
+        return result;
     }
 
     @Override
     public List<ViewCountDTO> getTotalCountWithDateRange(String campaignId, String sectionId, Date start, Date end) {
-        return null;
+        List<ViewCountDTO> result = null;
+        if (checkParams(campaignId, sectionId, start, end)) {
+            throw new NullPointerException("campaignId, sectionId, start, end   NULL veya bos olamaz!");
+        }
+        return result;
     }
+
 }
