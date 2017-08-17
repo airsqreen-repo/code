d2ffc8b580c1edcb5e44bb6793123e93c49342d0
benan <aktas.benan@gmail.com>
2017-08-17 23:19:41 +0300

    user insert hatası

--- backend/src/main/java/com/pusulait/airsqreen/service/system9/Sistem9Adapter.java
+++ backend/src/main/java/com/pusulait/airsqreen/service/system9/Sistem9Adapter.java
@@ -20,6 +20,10 @@ import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import javax.naming.MalformedLinkException;
+import javax.xml.ws.soap.AddressingFeature;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,7 +32,7 @@ import java.util.Optional;
  */
 @Slf4j
 @Service
-public class Sistem9Adapter extends WebServiceGatewaySupport {
+public class Sistem9Adapter {
 
     @Inject
     private Environment env;
@@ -36,55 +40,35 @@ public class Sistem9Adapter extends WebServiceGatewaySupport {
     @Autowired
     private PlatformUserRepository platformUserRepository;
 
-
     private String apiUrl;
-    private String appId;
-    private String sourceSys;
-
-    private WebServiceTemplate webServiceTemplate;
-    private S9WExSrvc s9WExSrvc;
-    private WinActionRelease winActionRelease;
+    S9WExSrvc service = null;
     private ObjectFactory objectFactory;
 
-    //@Inject
-    private Jaxb2Marshaller marshaller;
-
     @PostConstruct
-    public void init() {
+    public void init() throws MalformedURLException {
 
         this.apiUrl = env.getProperty("sistem9.api.url");
-        this.appId = env.getProperty("sistem9.api.application_id");
-        this.sourceSys = env.getProperty("sistem9.api.source_system");
-        this.setDefaultUri(apiUrl);
-
-    }
-
-    protected void initJaxbMarshaller() {
-        this.setMarshaller(marshaller);
-        this.setUnmarshaller(marshaller);
-        webServiceTemplate = getWebServiceTemplate();
+        service = new S9WExSrvc(new URL(apiUrl));
     }
 
 
     public void pushEvent(Sistem9PushEvent event) throws Exception {
 
-        WinActionRelease winActionRelease = objectFactory.createWinActionRelease();
+        WinActionTemplate winActionTemplate = objectFactory.createWinActionTemplate();
 
-        winActionRelease.setActionID(event.getActionId());
-        winActionRelease.setDeviceID(event.getDeviceId().intValue());
+        winActionTemplate.setActionID(event.getActionId());
+        winActionTemplate.setDeviceID(event.getDeviceId().intValue());
 
         PlatformUser platformUser = event.getDevice().getPlatformUser();
 
-        if (platformUser != null && platformUser.getPlatformType().equals(PlatformType.SSP) && platformUser.getServiceType().equals(ServiceType.SISTEM_9) ) {
+        if (platformUser != null && platformUser.getPlatformType().equals(PlatformType.SSP) && platformUser.getServiceType().equals(ServiceType.SISTEM_9)) {
 
-            winActionRelease.setPassword(platformUser.getPassword());
-            winActionRelease.setUserName(platformUser.getUsername());
+            winActionTemplate.setPassword(platformUser.getPassword());
+            winActionTemplate.setUserName(platformUser.getUsername());
         } else {
             throw new Exception("Sistem 9 tanımlı user yok Tanımlı User Yok");
         }
-
-        String response = s9WExSrvc.getS9WExSrvcSoap12().winActionRelease(winActionRelease.getUserName(), winActionRelease.getPassword(), winActionRelease.getActionID(),
-                winActionRelease.getDeviceID());
+        String response = service.getS9WExSrvcSoap12().winActionTemplate(winActionTemplate.getUserName(), winActionTemplate.getPassword(), winActionTemplate.getActionID(),winActionTemplate.getDeviceID());
 
     }
 }
